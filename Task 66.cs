8ea85d8df75ee8b640aebba33c4782e903d8bb36
Evgeniy <jenyasereb95@gmail.com>
2022-10-16 15:34:27 +0500

    66

---
+++ Task 66.cs
@@ -0,0 +1,26 @@
+﻿int GetNumberfromUser(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumbers (int m , int n)
+{
+   int sum =0;
+    
+    if(m < n)
+    {
+    
+        
+        sum = m + SumNumbers(m +1 , n);
+     
+    }
+    else if (m==n)
+    {
+        sum = sum + n;
+    }
+   return sum;
+}
+int n = GetNumberfromUser("Enter N");
+int m = GetNumberfromUser("Enter M");
+Console.Write(SumNumbers(m , n));
