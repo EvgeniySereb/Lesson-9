22e6323e937f6a82242c446b69117467f771cc77
Evgeniy <jenyasereb95@gmail.com>
2022-10-16 15:33:24 +0500

    64

---
+++ Task 64.cs
@@ -0,0 +1,18 @@
+﻿int GetNfromUser(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void RecursionN (int n )
+{
+    int number = 1;
+    if (n >= number)
+    {
+          Console.Write (n + " ");
+          RecursionN(n-1);
+
+    }
+}
+int n = GetNfromUser("Enter N");
+RecursionN(n);
